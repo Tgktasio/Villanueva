0d32054889e34aea1226cc02482e955275f2e604
Vin vin Vin <vvinvin809@gmail.com>
2024-05-21 18:54:01 +0800

    2&1

---
+++ app/src/main/java/com/example/villanueva/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.villanueva;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView textView1;
+    Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+            textView1 = findViewById(R.id.textView);
+
+            button = findViewById(R.id.button);
+
+            button.setOnClickListener(v -> {
+                Intent intent = new Intent (this, MainActivity2.class);
+                startActivity(intent);
+                finish();
+            });
+
+    }
+}
